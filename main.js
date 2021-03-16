970d3649a6a6ad7e2a3c504cdd3a1d4c873a8a52
alenterman <anouklenterman@icloud.com>
2021-03-16 20:33:00 +0100

    index

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ function klok(){
 
 	var klok = new Date();
 	var minutes = klok.getMinutes();
+	var uur = klok.getHours();
+
+	if (uur >= 9 && uur <= 21){
+		document.getElementById('blink').classList.remove('nacht');
+		document.getElementById('blink').classList.remove('dag');
+	} else {
+		document.getElementById('blink').classList.remove('dag');
+		document.getElementById('blink').classList.remove('nacht');
+	}
 
 
 	if (minutes < 10){
@@ -33,4 +42,36 @@ var today = new Date();
 var maanden = new Array ('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
 
 document.getElementById('datum').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()] + ' ' + today.getFullYear();
+
+
+
+
+
+
+
+setInterval(function(){
+	document.getElementById("blink").style="display:none";
+
+},1000);
+
+setInterval(function(){
+	document.getElementById("blink").style="display:block";
+
+},2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
